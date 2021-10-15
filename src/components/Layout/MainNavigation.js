058681b17a0a44eac1b1742a48c95f976229ebcd
nvmsridhar <nvmsridhar5@gmail.com>
2021-10-15 19:54:53 +0530

    7 files modified

--- src/components/Layout/MainNavigation.js
+++ src/components/Layout/MainNavigation.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import { Link } from 'react-router-dom';
 
 import classes from './MainNavigation.module.css';
@@ -5,16 +6,16 @@ import classes from './MainNavigation.module.css';
 const MainNavigation = () => {
   return (
     <header className={classes.header}>
-      <Link to='/'>
+      <Link to="/">
         <div className={classes.logo}>React Auth</div>
       </Link>
       <nav>
         <ul>
           <li>
-            <Link to='/auth'>Login</Link>
+            <Link to="/auth">Login</Link>
           </li>
           <li>
-            <Link to='/profile'>Profile</Link>
+            <Link to="/profile">Profile</Link>
           </li>
           <li>
             <button>Logout</button>
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import ReactDOM from 'react-dom';
+import React from 'react';import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 
 import './index.css';
